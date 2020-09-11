4375ad1dd27d86347a3f6a5b12e3a80156d6cac0
NivGorsky <gorsky7411@gmail.com>
2020-09-11 17:28:01 +0300

    Initial Commit

---
+++ Encyption project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Encyption project
+//
+//  Created by ניב גורסקי on 11/09/2020.
+//  Copyright © 2020 niv. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
